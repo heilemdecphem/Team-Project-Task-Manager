4b715daad06f6ef85614b0afdca6e292a62bb89d
heilemdecphem <phamduclamhai.1301@gmail.com>
2026-04-06 12:31:04 +0700

    Update task.h

--- task.h
+++ task.h
@@ -3,26 +3,22 @@
 
 #include <stdbool.h>
 
-// =====================
 // Struct Definition
-// =====================
 typedef struct Task {
     int id;
     char title[100];
     bool isImportant;
+    int order;
     struct Task* next;
 } Task;
 
-// =====================
-// Core Function Prototypes
-// =====================
 
+// Core Function Prototypes
 Task* createTask(int id, char* title, bool isImportant); // Create a new task node
 void deleteTask(Task** head, int id); // Delete a task by ID
 void displayTasks(Task* head); // Display all tasks
 Task* searchTask(Task* head, int id); // Search for a task by ID
 void sortTasks(Task* head); // Sort tasks (important first)
 void freeList(Task* head); // Free entire task list
-
 
 #endif
